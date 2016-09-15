e1da193e503fb3939ac6740381951ff09c915c2c
HandcraftedBits <opensource@handcraftedbits.com>
2016-09-15 11:19:51 -0400

    Fix issues with ssh-agent checking.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 # Start ssh-agent if it hasn't been started already.
 
-if test -z (command pgrep ssh-agent)
+set agent_running (pgrep ssh-agent)
+
+if test -z $agent_running
+     set -e SSH_AGENT_PID
+     set -e SSH_AUTH_SOCK
+
      eval (command ssh-agent -c | sed -E 's/^setenv (.+);$/set \1; set -Ux \1;/')
 end
