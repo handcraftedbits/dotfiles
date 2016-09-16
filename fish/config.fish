166d6d1f1a27092d032844639f7f0c27f8736d48
HandcraftedBits <opensource@handcraftedbits.com>
2016-09-16 09:32:25 -0400

    More fixes for ssh-agent environment variables.

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,5 @@
 set agent_running (pgrep ssh-agent)
 
 if test -z $agent_running
-     set -e SSH_AGENT_PID
-     set -e SSH_AUTH_SOCK
-
-     eval (command ssh-agent -c | sed -E 's/^setenv (.+);$/set \1; set -Ux \1;/')
+     eval (command ssh-agent -c | sed -E 's/^setenv ([^ ]+) (.+);$/set -e \1; set -e \1; set -Ux \1 \2;/')
 end
