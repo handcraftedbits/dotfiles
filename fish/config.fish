8ead29ce29b17bde01e3ade4dff08b46e30d52cc
HandcraftedBits <opensource@handcraftedbits.com>
2017-10-04 09:33:10 -0400

    Remove fish, add ZSH, tweak tmux escape time.

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-# Start ssh-agent if it hasn't been started already.
-
-set agent_running (pgrep ssh-agent)
-
-if test -z $agent_running
-     eval (command ssh-agent -c | sed -E 's/^setenv ([^ ]+) (.+);$/set -e \1; set -e \1; set -Ux \1 \2;/')
-end
